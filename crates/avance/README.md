b9afe2f51807fc2e63abc3ae2b73988158966aa2
Aubrey-Liu <aubreyliu233@gmail.com>
2023-07-01 08:35:45 +0800

    Updated README

--- crates/avance/README.md
+++ crates/avance/README.md
@@ -1,5 +1,8 @@
 A Rust library for easily displaying progress bar in command line applications.
 
+avance provides out of box progress bar utilites, which are lightweight, user-friendly
+and can be used fearlessly in a concurrent environment.
+
 [![Documentation](https://docs.rs/avance/badge.svg)](https://docs.rs/avance/)
 [![Crates.io](https://img.shields.io/crates/v/avance.svg)](https://crates.io/crates/avance)
 
--- crates/avance/examples/concurrent.rs
+++ crates/avance/examples/concurrent.rs
@@ -1,21 +1,24 @@
 use std::thread;
 use std::time::Duration;
 
-use avance::AvanceBar;
+use avance::*;
 
 fn main() {
     let total = 1000;
     let mut v = vec![0; total];
-    let pb1 = AvanceBar::new(total as u64).with_desc("multi");
+    let pb1 = AvanceBar::new(total as u64)
+        .with_style(Style::Balloon)
+        .with_desc("x8");
     std::thread::scope(|t| {
-        for chunk in v.chunks_mut(total / 4) {
+        for chunk in v.chunks_mut(total / 8) {
             t.spawn(|| {
                 for x in chunk {
                     *x = 1;
 
                     // Suppose we're doing some io tasks
-                    thread::sleep(Duration::from_millis(2));
+                    thread::sleep(Duration::from_millis(3));
 
+                    // You can use one progress bar fearlessly in multiple threads
                     pb1.inc();
                 }
             });
@@ -23,13 +26,13 @@ fn main() {
     });
     pb1.close();
 
-    let pb2 = AvanceBar::new(total as u64).with_desc("single");
+    let pb2 = pb1.clone().with_desc("x1");
     std::thread::scope(|t| {
         t.spawn(|| {
             for x in &mut v {
                 *x = 2;
 
-                thread::sleep(Duration::from_millis(2));
+                thread::sleep(Duration::from_millis(3));
 
                 pb2.inc();
             }
